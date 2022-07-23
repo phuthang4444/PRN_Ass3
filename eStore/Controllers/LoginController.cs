321fe92614c99ed621a8310b05926d73b977d319
Trường Nguyễn <sagemode771@gmail.com>
2022-07-23 23:32:02 +0700

    ThangTrieu Commit

--- eStore/Controllers/LoginController.cs
+++ eStore/Controllers/LoginController.cs
@@ -26,13 +26,13 @@ namespace eStore.Controllers
         {
             if (member.Email != null && member.Password != null)
             {
-                Member loggedInUser = memberRepository.Login(member.Email, member.Password);
                 if (member.Email.ToLower().Equals("admin@fstore.com") && member.Password.Equals("admin@@"))
                 {
                     HttpContext.Session.SetInt32("ID", 1);
                     return RedirectToAction("Index", "Home"); 
 
                 }
+                Member loggedInUser = memberRepository.Login(member.Email, member.Password);
                 if (loggedInUser != null)
                 {
                     HttpContext.Session.SetInt32("ID", loggedInUser.MemberId);
