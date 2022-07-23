d821218cad2eb4fdafbab241d1686de668db1eb2
phuthang4444 <thangtpse151058@fpt.edu.vn>
2022-07-23 21:34:52 +0700

    fix bug Login Controller and add Member Controlelr, Product Controller

--- BusinessObejct/Object/Product.cs
+++ BusinessObejct/Object/Product.cs
@@ -12,6 +12,6 @@ namespace BusinessObejct.Object
         public string ProductName { get; set; }
         public string Weight { get; set; }
         public decimal UnitPrice { get; set; }
-        public int UnitsInStock { get; set; }
+        public int UnitslnStock { get; set; }
     }
 }
